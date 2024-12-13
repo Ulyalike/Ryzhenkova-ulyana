3185cbfa13d863eab3545a4b8fbb09d9067ab648
Ulyalike <ryzhenkova.ue@phystech.edu>
2024-12-14 00:12:50 +0300

    Create CommentIdDuplicatedException.java

---
+++ src/main/java/repository/exceptions/CommentIdDuplicatedException.java
@@ -0,0 +1,12 @@
+package repository.exceptions;
+
+public class CommentIdDuplicatedException extends RuntimeException {
+
+  public CommentIdDuplicatedException(String message) {
+    super(message);
+  }
+
+  public CommentIdDuplicatedException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
