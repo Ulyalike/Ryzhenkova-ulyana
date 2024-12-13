85b718e3ec8e150ac90f29f88debb32599b38c6a
Ulyalike <ryzhenkova.ue@phystech.edu>
2024-12-14 00:17:41 +0300

    Create ArticleUpdateException.java

---
+++ src/main/java/service/exceptions/ArticleUpdateException.java
@@ -0,0 +1,12 @@
+package service.exceptions;
+
+public class ArticleUpdateException extends RuntimeException {
+
+  public ArticleUpdateException (String message) {
+    super(message);
+  }
+
+  public ArticleUpdateException (String message, Throwable cause) {
+    super(message, cause);
+  }
+}
