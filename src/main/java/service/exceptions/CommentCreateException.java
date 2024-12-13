9ceefa87f2f84cdc70c127c939cef237af429a71
Ulyalike <ryzhenkova.ue@phystech.edu>
2024-12-14 00:16:57 +0300

    Create CommentCreateException.java

---
+++ src/main/java/service/exceptions/CommentCreateException.java
@@ -0,0 +1,12 @@
+package service.exceptions;
+
+public class CommentCreateException extends RuntimeException {
+
+  public CommentCreateException (String message) {
+    super(message);
+  }
+
+  public CommentCreateException (String message, Throwable cause) {
+    super(message, cause);
+  }
+}
